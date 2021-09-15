b8521da10bfd84002c4f9b8f89115230c82fa76a
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2021-09-15 15:33:26 +0000

    first commit

--- lib/cdk-rds-serverless-stack.ts
+++ lib/cdk-rds-serverless-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from '@aws-cdk/core';
 
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as rds from '@aws-cdk/aws-rds';
+
 export class CdkRdsServerlessStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    
+    const vpc = new ec2.Vpc(this, 'MyVPC');
+    
+    const cluster = new rds.ServerlessCluster(this, 'ServerlessCluster', {
+      engine: rds.DatabaseClusterEngine.AURORA_MYSQL,
+      vpc,
+      enableDataApi: true,   // Optional - will be automatically set if you call grantDataApiAccess()
+    });
+    
+    
+    
   }
 }
